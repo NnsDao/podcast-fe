3352e52b782ac257b57d7c760740b071babcd024
neptune <103205025+neptune0xf@users.noreply.github.com>
2023-01-03 16:53:46 +0800

    fix: upload file

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,9 +1,9 @@
-import fleekStorage from '@fleekhq/fleek-storage-js';
 import { CloudUploadOutlined } from '@mui/icons-material';
-import { Avatar, Paper, Stack } from '@mui/material';
+import { Avatar, CircularProgress, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import { Box } from '@mui/system';
 import React from 'react';
+import { useToggle } from 'usehooks-ts';
 
 function readFileContents(file) {
   return new Promise(resolve => {
@@ -14,64 +14,76 @@ function readFileContents(file) {
 }
 
 function Upload(props) {
+  const { src, setSrc } = props;
   const uploaderRef = React.useRef<any>();
-  const [src, setSrc] = React.useState('');
+  const [progress, setProgress] = React.useState(0);
+  const [isUploading, toggleIsUploading] = useToggle(false);
   async function uploadFile() {
-    const file = uploaderRef.current.files[0];
-    const buffer = await readFileContents(file);
-
-    const uploadedFile = await fleekStorage.upload({
-      apiKey: 'tY/tQQtffm+CEqHFHmQXqQ==',
-      apiSecret: 'fgjadsHiuaBA5kT1QSlVIcemrp97XZWW9sEBL6YS3+E=',
-      key: (Date.now() * Math.random() * 1e4).toString(16),
-      // @ts-ignore
-      ContentType: file.type,
-      data: buffer,
-      httpUploadProgressCallback: event => {
-        console.log(Math.round((event.loaded / event.total) * 100) + '%');
-      },
+    setSrc('');
+    toggleIsUploading();
+    const form = new FormData();
+    [...uploaderRef.current.files].map(file => {
+      form.append('file', file);
     });
-    //
-    setSrc(`${uploadedFile.publicUrl}?hash=${uploadedFile.hash}`);
-    console.log('file url', uploadedFile);
+    let uploadedFile = await fetch('https://napi.icmarket.ooo/public/file/upload', {
+      body: form,
+      method: 'POST',
+    });
+    uploadedFile = await uploadedFile.json();
+    // @ts-ignore
+    const fileURL = uploadedFile.data?.data?.[0];
+
+    // @ts-ignore
+    console.log('file url', uploadedFile, fileURL);
+    setSrc(fileURL);
+    toggleIsUploading();
+    setProgress(0);
   }
-  const UploadButton = () => {
-    return (
-      <IconButton color="primary" size="large" component="label">
-        <CloudUploadOutlined fontSize="inherit"></CloudUploadOutlined>
-        <input
-          type="file"
-          name="uploader"
-          ref={uploaderRef}
-          id="uploader"
-          hidden
-          accept="image/*"
-          onChange={uploadFile}
-        />
-      </IconButton>
-    );
-  };
-  if (!src) {
-    return (
-      <Paper
-        sx={{
-          width: '100px',
-          height: '100px',
-          textAlign: 'center',
-        }}>
-        <Stack justifyContent="center" direction="column" spacing={0} alignItems="center" height="100%">
-          <UploadButton></UploadButton>
-          <Typography variant="body1">upload</Typography>
-        </Stack>
-      </Paper>
-    );
-  }
+
   return (
-    <Paper sx={{ position: 'relative' }}>
-      <Avatar src={src} sizes="large"></Avatar>
-      <UploadButton />
-    </Paper>
+    <IconButton disableRipple color="primary" size="large" component="label" sx={{ width: '108px', height: '108px' }}>
+      {src ? (
+        <Avatar src={src} sizes="large" sx={{ width: '108px', height: '108px' }}></Avatar>
+      ) : isUploading ? (
+        <UploadingProgress></UploadingProgress>
+      ) : (
+        <Avatar sx={{ width: '108px', height: '108px' }}>
+          <CloudUploadOutlined sx={{ width: '48px', height: '48px' }}></CloudUploadOutlined>
+        </Avatar>
+      )}
+      <input
+        type="file"
+        multiple
+        name="uploader"
+        ref={uploaderRef}
+        id="uploader"
+        hidden
+        accept="image/*"
+        onChange={uploadFile}
+      />
+    </IconButton>
   );
+
+  function UploadingProgress() {
+    return (
+      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+        <CircularProgress variant="determinate" value={progress} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Typography variant="caption" component="div" color="text.secondary">{`${progress}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
 }
 
 export default Upload;
