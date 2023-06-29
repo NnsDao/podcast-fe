73bf3614e9c2754202656074a9bc5f4608aa0f70
chuhemiao <535101602@qq.com>
2023-06-29 17:40:07 +0800

    feat: manager

--- src/pages/padcast/status/Index.tsx
+++ src/pages/padcast/status/Index.tsx
@@ -1,20 +1,72 @@
+import { useUpgrade_podcast } from '@/api/manage';
 import { useDeposit } from '@/api/podcast';
+import { useUserStore } from '@/hooks/userStore';
 import { Principal } from '@dfinity/principal';
-import { Button, Card, Divider, TextField } from '@mui/material';
-import { Stack } from '@mui/system';
+import DeleteIcon from '@mui/icons-material/Delete';
+import StopIcon from '@mui/icons-material/Stop';
+import UpgradeIcon from '@mui/icons-material/Upgrade';
+import {
+  Button,
+  Card,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  Stack,
+  TextField,
+} from '@mui/material';
 import Grid from '@mui/system/Unstable_Grid/Grid';
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import toast from 'react-hot-toast';
+
 import { useParams } from 'react-router-dom';
 import List from './list/Index';
 
 export default function Status() {
   const { cid } = useParams();
   const { principal } = useParams();
+  const [open, setOpen] = useState(false);
+  const [userStore, dispatch] = useUserStore();
+  const isLogin = userStore.isLogin;
+  const [isShowDialog, setIsShowDialog] = useState(false);
 
-  console.log(principal, 898989);
-
+  const upgradePodcastAction = useUpgrade_podcast(Principal.fromText(principal as string));
   const AddDepositAction = useDeposit(principal as string);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  async function newUpgrade() {
+    handleClose();
+    if (!isLogin) {
+      toast.error('Login first please!');
+      setIsShowDialog(true);
+      return;
+    }
+
+    const toastID = toast.loading('Upgrade new wasm code...');
+
+    try {
+      //@ts-ignore
+      const data = await upgradePodcastAction.mutateAsync();
+      console.log(data);
+      toast.success('Successfully!');
+    } catch (error) {
+      console.error('err', error);
+      toast.error('Failed create');
+    } finally {
+      toast.dismiss(toastID);
+    }
+  }
+
+  // todo delete stop
 
   return (
     <Grid sm={12}>
@@ -28,12 +80,54 @@ export default function Status() {
       <Grid container sm={12}>
         <List></List>
       </Grid>
+
+      <Stack direction={'row'} className="py-8 items-center">
+        <Stack sx={{ fontWeight: '900', fontSize: '22px', marginRight: '20px' }}>Mangage</Stack>
+        <Button
+          sx={{ fontWeight: '500', marginRight: '20px' }}
+          variant="outlined"
+          color="secondary"
+          onClick={() => handleClickOpen()}>
+          <UpgradeIcon /> Upgrade Podcast
+        </Button>
+
+        <Button
+          sx={{ fontWeight: '500', marginRight: '20px' }}
+          variant="outlined"
+          color="secondary"
+          onClick={() => handleClickOpen()}>
+          <StopIcon /> Stop Podcast
+        </Button>
+
+        <Button
+          sx={{ fontWeight: '500', marginRight: '20px' }}
+          variant="outlined"
+          color="secondary"
+          onClick={() => handleClickOpen()}>
+          <DeleteIcon /> Delete Podcast
+        </Button>
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">{'upgrade for new podcast'}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">upgrade maybe lost old data.</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Disagree</Button>
+            <Button onClick={newUpgrade} autoFocus>
+              Agree Upgrade
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Stack>
     </Grid>
   );
 
   function AddCycle(props) {
-    console.log(props, 282828);
-
     function changeForm(key, e) {
       console.log(key, e);
       setFormField({ key, value: e.target.value });
@@ -55,7 +149,6 @@ export default function Status() {
       const toastID = toast.loading('Getting Add cycle...');
       try {
         const { AddCycle } = form;
-        console.log(AddCycle, 90909090);
         //@ts-ignore
         const data = await AddDepositAction.mutateAsync({
           arg_0: await Principal.fromText(principal as string),
